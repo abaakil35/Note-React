4c3af276deb52896a53836b7737e75336075bb2a
Abaakil-Ayoub <154565344+Abaakil35@users.noreply.github.com>
2024-12-07 02:53:57 +0100

    some changes !

---
+++ src/Components/ReceivedNotes.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Style/ReceivedNotes.css";
+import { useState, useEffect } from "react";
+// import axios from "axios";
+
+const ReceivedNotes = () => {
+
+    return
+    
+};
